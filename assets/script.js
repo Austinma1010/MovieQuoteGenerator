7db9ce279129c12cf65c7f9d0f6319dfef9bdba4
Austinma10 <austinma2168@gmail.com>
2023-12-21 20:52:25 -0500

    Added javascript functionality to search button

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
-var search = document.getElementById('movie-list');
-
+var search = document.getElementById('movie-input');
+var searchBtn = document.getElementById('search-btn');
+var movieInfo = document.getElementById('movieInfo');
 function DisplayMovieInfo() {
     var requestUrl = "http://www.omdbapi.com/?apikey=e8bcf7cb&t=" + search.value;
   
@@ -8,6 +9,17 @@ function DisplayMovieInfo() {
       return response.json();
     })
     .then(function (data) {
-      
+      var title = movieInfo.appendChild(document.createElement('li'));
+      var releaseDate = movieInfo.appendChild(document.createElement('li'));
+      var mainActors = movieInfo.appendChild(document.createElement('li'));
+      var revenue = movieInfo.appendChild(document.createElement('li'));
+      var plot = movieInfo.appendChild(document.createElement('li'));
+      title.textContent = "Title: " + data.Title;
+      releaseDate.textContent = "Release Date: " + data.Released;
+      mainActors.textContent = "Main Cast: " + data.Actors;
+      revenue.textContent = "Box Office Revenue: " + data.BoxOffice;
+      plot.textContent = "Plot Summary: " + data.Plot;
   }) }
 
+
+searchBtn.addEventListener('click', DisplayMovieInfo);
