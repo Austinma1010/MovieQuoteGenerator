fdbe67389bdfe046723bc114afabc8a3dd49dd22
Jordan <jkimys2@gmail.com>
2024-01-04 19:23:58 -0500

    readme file push

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# WordInfo-project
+# Movie-Search-Engine
+
+
+## Description
+
+This project allows users to search for movies, and when they do, they are presented with information about that movie. Following their search, the user is then able to get more information about that movie by clicking the "More Info" button, where they are redirected to the Wikipedia page for that movie. They are also able to save and view their searches as well. This webpage was created for all movie lovers that want to learn more about their favorite movies! 
+
+The challenge was created by following the acceptance criterias listed below:
+
+```
+GIVEN a movie search engine
+WHEN I search a movie
+THEN I am presented with the title of the movie as well as the release date, main actors, revenue, and the plot of the movie
+WHEN I press the save movie button
+THEN the movie title is saved into the local storage
+WHEN I hover over the saved movies list
+THEN I am presented with the saved movies 
+WHEN I click on a saved movie title
+THEN I am presented with information about the movie
+WHEN I click the more information button
+THEN I am redirected to the Wikipedia page for that movie
+```
+
+## Usage
+
+This webapage can be used by entering a movie title and clicking the search button. The user can then click on the save movie or more information buttons in order to navigate through the page. The user can search and save as many movies as they prefer. Below is an example of how the page appears:
+
+
+![screenshot](./assets/final-screenshot.png)
+
+
+## Technologies Used
+
+Bulma<br>
+jQuery<br>
+OMDB API <br>
+Wikipedia API<br>
+
+
+## Links
+
+GitHub Repository: https://github.com/Austinma1010/MovieSearchEngine
+
+Deployed Application: https://austinma1010.github.io/MovieSearchEngine/
--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,11 @@ var searchBtn = document.getElementById("search-btn");
 var movieInfo = document.getElementById("movieInfo");
 var dropDownEl = document.getElementById("drop-down");
 //var viewSavedbtn = document.getElementById("view-saved")
-var movieNameArray = JSON.parse(localStorage.getItem('movieName')) || [];
-var clearSavedBtn = document.getElementById("clear-saved")
-
+var movieNameArray = JSON.parse(localStorage.getItem("movieName")) || [];
+var clearSavedBtn = document.getElementById("clear-saved");
 
 function DisplayMovieInfo(name) {
-    var requestUrl = "http://www.omdbapi.com/?apikey=e8bcf7cb&t=" + name;
+  var requestUrl = "http://www.omdbapi.com/?apikey=e8bcf7cb&t=" + name;
 
   fetch(requestUrl)
     .then(function (response) {
@@ -16,87 +15,84 @@ function DisplayMovieInfo(name) {
     })
     .then(function (data) {
       movieInfo.innerHTML = "";
-      var title = movieInfo.appendChild(document.createElement('li'));
-      var releaseDate = movieInfo.appendChild(document.createElement('li'));
-      var mainActors = movieInfo.appendChild(document.createElement('li'));
-      var revenue = movieInfo.appendChild(document.createElement('li'));
-      var plot = movieInfo.appendChild(document.createElement('li'));
+      var title = movieInfo.appendChild(document.createElement("li"));
+      var releaseDate = movieInfo.appendChild(document.createElement("li"));
+      var mainActors = movieInfo.appendChild(document.createElement("li"));
+      var revenue = movieInfo.appendChild(document.createElement("li"));
+      var plot = movieInfo.appendChild(document.createElement("li"));
       title.textContent = "Title: " + data.Title;
-      title.setAttribute('class', 'movieTitle');
+      title.setAttribute("class", "movieTitle");
       releaseDate.textContent = "Release Date: " + data.Released;
       mainActors.textContent = "Main Cast: " + data.Actors;
       revenue.textContent = "Box Office Revenue: " + data.BoxOffice;
       plot.textContent = "Plot Summary: " + data.Plot;
-      movieInfo.setAttribute('class', 'movieStyle');
+      movieInfo.setAttribute("class", "movieStyle");
       showWiki(name);
-  })
+    });
 }
 
-searchBtn.addEventListener('click', function() {
-
+searchBtn.addEventListener("click", function () {
   if (search.value == 0) {
     return;
   } else {
-
     DisplayMovieInfo(search.value);
   }
 });
 
 function showWiki(name) {
-  var requestUrl = "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=" + name + " movie&limit=1&format=json&formatversion=2";
+  var requestUrl =
+    "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=" +
+    name +
+    " movie&limit=1&format=json&formatversion=2";
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      if (data[3].includes('https')) {
-        
-      var wiki = document.getElementById('wiki-btn');
-      wiki.setAttribute('href', data[3]);
-  
-      return;
-    }
-      else { 
-        var requestUrl2 = "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=" + name + "&limit=1&format=json&formatversion=2";
+      if (data[3].includes("https")) {
+        var wiki = document.getElementById("wiki-btn");
+        wiki.setAttribute("href", data[3]);
 
-  fetch(requestUrl2)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      
-      wiki = document.getElementById('wiki-btn');
-      wiki.setAttribute('href', data[3]);
-       
-      return;
-})
+        return;
+      } else {
+        var requestUrl2 =
+          "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=" +
+          name +
+          "&limit=1&format=json&formatversion=2";
 
+        fetch(requestUrl2)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            wiki = document.getElementById("wiki-btn");
+            wiki.setAttribute("href", data[3]);
+
+            return;
+          });
       }
-})
+    });
 }
 
-
 //save movie title to local storage
-$('#save-btn').on('click', function(){
-  var newInput = $('#movie-input').val();
-  if (!movieNameArray.includes(newInput)){
-    movieNameArray.push(newInput)
-    localStorage.setItem('movieName',JSON.stringify(movieNameArray));
+$("#save-btn").on("click", function () {
+  var newInput = $("#movie-input").val();
+  if (!movieNameArray.includes(newInput)) {
+    movieNameArray.push(newInput);
+    localStorage.setItem("movieName", JSON.stringify(movieNameArray));
   }
   dropDownEl.innerHTML = "";
   viewSaved();
 });
 //view saved movies via dropdown
-function viewSaved (){
- // $('#dropdown-menu4').empty();
-  for (var i=0; i < movieNameArray.length; i++){
-  
-    var savedMovieTit = dropDownEl.appendChild(document.createElement('li'));
-    savedMovieTit.setAttribute('class', "dropDownListEl")
+function viewSaved() {
+  // $('#dropdown-menu4').empty();
+  for (var i = 0; i < movieNameArray.length; i++) {
+    var savedMovieTit = dropDownEl.appendChild(document.createElement("li"));
+    savedMovieTit.setAttribute("class", "dropDownListEl");
     savedMovieTit.textContent = movieNameArray[i];
-    savedMovieTit.addEventListener('click', clickSaved);
-    
+    savedMovieTit.addEventListener("click", clickSaved);
   }
 }
 
@@ -105,8 +101,7 @@ function clickSaved() {
 }
 viewSaved();
 
-
-clearSavedBtn.addEventListener("click", function(event) {
+clearSavedBtn.addEventListener("click", function (event) {
   event.preventDefault();
   localStorage.removeItem("movieName");
   movieNameArray = [];
